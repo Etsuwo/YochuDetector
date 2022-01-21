c1c01c8e45d8174b632787593f8c3f05f0908ae1
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-22 00:32:03 +0900

    [add] プロジェクト作成

---
+++ YochuDetector2/YochuDetector2App.swift
@@ -0,0 +1,17 @@
+//
+//  YochuDetector2App.swift
+//  YochuDetector2
+//
+//  Created by Etsushi Otani on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct YochuDetector2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
