ec048063ed4ef9b2a05b61c3a8cbebaf47e02480
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-25 01:19:19 +0900

    [add] StringにisNotEmpty追加

---
+++ YochuDetector2/Extension/String+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extension.swift
+//  YochuDetector2
+//
+//  Created by Etsushi Otani on 2022/01/25.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        !isEmpty
+    }
+}
