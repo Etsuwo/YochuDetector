ff7c9c17bd784e5efb6957e52f2f78fc726f19b7
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-25 01:34:15 +0900

    [add] TrimmingView追加

---
+++ YochuDetector2/TrimmingView.swift
@@ -0,0 +1,20 @@
+//
+//  TrimmingView.swift
+//  YochuDetector2
+//
+//  Created by Etsushi Otani on 2022/01/25.
+//
+
+import SwiftUI
+
+struct TrimmingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrimmingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrimmingView()
+    }
+}
