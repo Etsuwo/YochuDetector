84772bec0c401ad0077b1d7c03a608a2df337b97
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-27 16:14:16 +0900

    [add] NotificationName追加

---
+++ YochuDetector2/Common/NotificationName.swift
@@ -0,0 +1,22 @@
+//
+//  NotificationName.swift
+//  YochuDetector2
+//
+//  Created by Etsushi Otani on 2022/01/27.
+//
+
+import Foundation
+enum NotificationName: String {
+    case transitionTrimming
+}
+
+extension Notification.Name {
+    static let didUserLoggedIn = NotificationName.transitionTrimming.name
+}
+
+extension NotificationName {
+    var name: Notification.Name {
+        let name = "\(NotificationCenter.default).\(rawValue)"
+        return Notification.Name(rawValue: name)
+    }
+}
