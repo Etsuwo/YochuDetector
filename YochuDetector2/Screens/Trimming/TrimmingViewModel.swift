38f544be31fa2e5c61eeba14f78b49caeedb1843
Etsushi <etsushi.otani@emoshu.co.jp>
2022-01-29 12:11:15 +0900

    [add] TrimmingViewModel作成

---
+++ YochuDetector2/Screens/Trimming/TrimmingViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TrimmingViewModel.swift
+//  YochuDetector2
+//
+//  Created by Etsushi Otani on 2022/01/29.
+//
+
+import Foundation
+import Combine
+
+final class TrimmingViewModel {
+    
+    init() {
+        
+    }
+}
