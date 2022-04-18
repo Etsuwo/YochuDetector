99891d54d9aecf3bbee26ffe27d7c1dd6ebb52d9
Etsushi <etsushi.otani@emoshu.co.jp>
2022-04-18 16:04:18 +0900

    [update] エサ領域除外機能実装

--- YochuDetector2/Common/Analyze/AreaExtractor.swift
+++ YochuDetector2/Common/Analyze/AreaExtractor.swift
@@ -6,8 +6,30 @@
 //
 
 import Foundation
+import AppKit
 import opencv2
 
 final class AreaExtractor {
-    
+    func extractFeed(from image: NSImage) -> NSImage {
+        let mat = Mat(nsImage: image)
+        let resultMat = Mat()
+        var channels: [Mat] = []
+        let areaThreshold = image.size.height * image.size.width * 0.05
+        let binaryThreshold: Double = 55 //TODO: 光と相談
+        
+        Imgproc.cvtColor(src: mat, dst: resultMat, code: .COLOR_RGB2HSV)
+        Imgproc.GaussianBlur(src: resultMat, dst: resultMat, ksize: Size2i(width: 9, height: 9), sigmaX: 0)
+        Core.split(m: resultMat, mv: &channels)
+        Imgproc.threshold(src: channels[1], dst: resultMat, thresh: binaryThreshold, maxval: 255, type: ThresholdTypes.THRESH_BINARY)
+        var contours: [[Point2i]] = []
+        let hierarchy = Mat()
+        Imgproc.findContours(image: resultMat, contours: &contours, hierarchy: hierarchy, mode: RetrievalModes.RETR_LIST, method: ContourApproximationModes.CHAIN_APPROX_NONE)
+        contours.forEach { contour in
+            let contourMat = MatOfPoint(array: contour)
+            if Imgproc.contourArea(contour: contourMat) > areaThreshold {
+                Imgproc.fillPoly(img: mat, pts: [contour], color: Scalar(0))
+            }
+        }
+        return mat.toNSImage()
+    }
 }
